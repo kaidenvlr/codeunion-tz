0c3dc0494a2747cadb46c84ad91e671775c548ad
kaidenvlr <kaidenvlr@gmail.com>
2023-11-03 23:43:44 +0600

    environment

--- core/settings.py
+++ core/settings.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from celery.schedules import crontab
 from dotenv import load_dotenv
 
-load_dotenv('.env_example')
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
