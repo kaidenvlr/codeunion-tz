8ed26756247e59b86d348d196d8a305d80f8b9a5
kaidenvlr <kaidenvlr@gmail.com>
2023-11-03 21:27:23 +0600

    renamed page size argument

--- apps/parser/classes.py
+++ apps/parser/classes.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 
 class Pagination(PageNumberPagination):
     page_size = 5
-    page_size_query_param = "page_size"
+    page_size_query_param = "per_page"
     max_page_size = 10
     page_query_param = "page"
 
