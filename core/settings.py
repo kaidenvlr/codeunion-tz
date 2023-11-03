592792a3fe31b29b99b20186437db542eee9d83e
kaidenvlr <kaidenvlr@gmail.com>
2023-11-03 21:48:54 +0600

    authentication + rest_framework settings

--- core/settings.py
+++ core/settings.py
@@ -120,6 +120,17 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# Rest Framework
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'apps.parser.authentication.BearerAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
+}
+
 # Superuser credentials for creating it.
 
 SUPERUSER_USERNAME = os.getenv("SUPERUSER_USERNAME")
